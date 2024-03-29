808b6b11dc4f25cf85d00f3917fb876287c8c95a
Ameya Padole <ameyapadole26@gmail.com>
2024-03-29 12:11:41 -0700

    Time: 101 ms (93.62%) | Memory: 19.3 MB (19.64%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
-        seen = defaultdict(int)
-        for num in nums:
-            seen[num] += 1
+        counter = collections.Counter(nums)
 
-        for num in seen:
-            if seen[num] == 1:
+        for num, count in counter.items():
+            if count == 1:
                 return num
-        
