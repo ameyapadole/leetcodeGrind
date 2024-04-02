d74ec2f1912b881dbe83dd0d4bc293970d680625
Ameya Padole <ameyapadole26@gmail.com>
2024-04-02 12:33:35 -0700

    Time: 43 ms (17.70%) | Memory: 16.6 MB (49.29%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -2,21 +2,11 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         l = 0 
         r = len(nums)
-        while l < r: 
-            if nums[l] == val: 
-                nums[l] = nums[r - 1] 
-                #Swap Current Element with the last element.
+
+        while l < r:
+            if nums[l] == val:
+                nums[l] = nums[r - 1]
                 r -= 1
-            else:
+            else: 
                 l += 1
         return r
-
-        """
-        Time complexity : O(n).
-        
-        Both l and r traverse at most r steps. 
-        In this approach, the number of assignment operations is equal to the number of elements to remove. 
-        So it is more efficient if elements to remove are rare.
-
-        Space complexity : O(1).
-        """
