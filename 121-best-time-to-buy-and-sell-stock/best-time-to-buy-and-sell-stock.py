79989261a53f235c24415961f5d326927938024a
Ameya Padole <ameyapadole26@gmail.com>
2024-04-03 13:23:49 -0700

    Time: 761 ms (49.55%) | Memory: 27.5 MB (11.72%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+
         maxprofit = 0 
         minprice = float('inf')
 
-        for price in prices:
+        for price in prices: 
             minprice = min(minprice, price)
             profit = price - minprice
             maxprofit = max(maxprofit, profit)
-        
         return maxprofit
-        
+
+        #Time Complexity = O(N)
+        #Space Complexity = O(1)        
