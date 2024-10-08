3190b3a3a5d8f8c2b2793391ff5c7400c984a0cc
Ameya Padole <ameyapadole26@gmail.com>
2024-10-08 15:21:37 -0700

    Time: 42 ms (17.06%) | Memory: 16.6 MB (57.92%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -6,14 +6,12 @@
 #         self.right = right
 class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        
-        if not root: 
+        stack = [(root, str(root.val))]
+        paths = []
+
+        if not root:
             return []
         
-        paths = []
-        stack = [(root, str(root.val))]
-        
-
         while stack: 
             node, path = stack.pop()
             if not node.left and not node.right:
@@ -23,4 +21,3 @@ class Solution:
             if node.right:
                 stack.append((node.right, path + '->' + str(node.right.val)))
         return paths
-        
