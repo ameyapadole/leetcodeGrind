bf90bb84792e9e7de12da091439305327e2285a2
Ameya Padole <ameyapadole26@gmail.com>
2024-10-08 15:25:33 -0700

    Time: 32 ms (73.22%) | Memory: 16.4 MB (60.28%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -0,0 +1,11 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        defanged = ""
+
+        for c in address: 
+            if c == '.':
+                defanged += "[.]"
+            else:
+                defanged += c
+        return defanged 
+        
