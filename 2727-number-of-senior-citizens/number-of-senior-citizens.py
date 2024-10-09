002f20440973da2f56088f07c43c73a64420d59a
Ameya Padole <ameyapadole26@gmail.com>
2024-10-08 20:58:17 -0700

    Time: 55 ms (6.51%) | Memory: 16.5 MB (41.62%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.py
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -4,6 +4,5 @@ class Solution:
         for detail in details:
             if detail[11:13] > "60":
                 count += 1            
-            else:
-                continue
+            
         return count
