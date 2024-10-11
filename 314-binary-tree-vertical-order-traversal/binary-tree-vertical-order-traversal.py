077796a1102c48b168ae295c1a2abaeb23357122
Ameya Padole <ameyapadole26@gmail.com>
2024-10-10 22:43:18 -0700

    Time: 33 ms (82.09%) | Memory: 16.6 MB (14.36%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -1,29 +1,28 @@
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-from collections import defaultdict
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
 
         if root is None:
             return []
-
-        columnTable = defaultdict(list)
+        
         queue = deque([(root, 0)])
-        min_column = max_column = 0
+        columnTable = defaultdict(list) 
+        min_cols = max_cols = 0 
 
-        while queue:
+        while queue: 
             node, column = queue.popleft()
 
             if node is not None:
                 columnTable[column].append(node.val)
-                min_column = min(min_column, column)
-                max_column = max(max_column, column)
+                min_cols = min(column, min_cols)
+                max_cols = max(column, max_cols)
 
                 queue.append((node.left, column - 1))
                 queue.append((node.right, column + 1))
-
-        return [columnTable[x] for x in range(min_column, max_column +1)]
+        return [columnTable[x] for x in range(min_cols, max_cols + 1)]
+        
