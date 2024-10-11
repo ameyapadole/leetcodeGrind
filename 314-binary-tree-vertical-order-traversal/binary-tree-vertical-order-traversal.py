22054e4584fe01ad01bbde3b4a31d5d860fa2d95
Ameya Padole <ameyapadole26@gmail.com>
2024-10-10 22:38:25 -0700

    Time: 34 ms (77.18%) | Memory: 16.6 MB (14.36%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -8,15 +8,22 @@ from collections import defaultdict
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
 
+        if root is None:
+            return []
+
         columnTable = defaultdict(list)
         queue = deque([(root, 0)])
+        min_column = max_column = 0
 
         while queue:
             node, column = queue.popleft()
 
             if node is not None:
                 columnTable[column].append(node.val)
+                min_column = min(min_column, column)
+                max_column = max(max_column, column)
+
                 queue.append((node.left, column - 1))
                 queue.append((node.right, column + 1))
 
-        return [columnTable[x] for x in sorted(columnTable.keys())]
+        return [columnTable[x] for x in range(min_column, max_column +1)]
