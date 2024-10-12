0337500be25891f622140924e0a93fb148e5e91b
Ameya Padole <ameyapadole26@gmail.com>
2024-10-11 20:42:48 -0700

    Time: 675 ms (69.11%) | Memory: 20.7 MB (40.13%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -5,20 +5,22 @@ class Solution:
         for i, num in enumerate(nums):
             if i > 0 and num == nums[i - 1]:
                 continue
-
             l = i + 1
-            r = len(nums) - 1 
-        
+            r = len(nums) - 1
             while l < r: 
-                threesum = num + nums[l] + nums[r]
-                if threesum > 0: 
-                    r -= 1
-                elif threesum < 0:
+                threesum = nums[l] + nums[r] + num
+                if threesum < 0:
                     l += 1
+                elif threesum > 0:
+                    r -= 1
                 else:
-                    res.append([num, nums[l], nums[r]]) 
+                    res.append([num, nums[l], nums[r]])
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-        return res
+        return res 
+
+
+                
+        
         
