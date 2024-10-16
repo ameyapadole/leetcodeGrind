15b9dec2d6b9ec69c9b597f7c36a951a3099b922
Ameya Padole <ameyapadole26@gmail.com>
2024-10-16 14:31:11 -0700

    Time: 71 ms (85.67%) | Memory: 19.4 MB (57.88%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,20 +5,21 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
-            return None
+        if len(lists) == 0 or not lists:
+            return None 
+
         while len(lists) > 1:
             mergedLists = []
-            for i in range(0, len(lists), 2):
+            for i in range(0 , len(lists), 2):
                 l1 = lists[i]
-                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None 
                 mergedLists.append(self.mergeList(l1, l2))
             lists = mergedLists
         return lists[0]
     
     def mergeList(self, l1, l2):
         dummy = ListNode()
-        tail = dummy
+        tail = dummy 
 
         while l1 and l2: 
             if l1.val < l2.val:
@@ -27,12 +28,13 @@ class Solution:
             else:
                 tail.next = l2
                 l2 = l2.next
+                
             tail = tail.next
-        
         if l1:
-            tail.next = l1
+            tail.next = l1 
         if l2:
             tail.next = l2
         
         return dummy.next
+
         
