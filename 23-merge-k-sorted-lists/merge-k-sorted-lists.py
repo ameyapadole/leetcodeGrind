6280748e7f226b1ff01620d1aa6454d712490565
Ameya Padole <ameyapadole26@gmail.com>
2024-10-16 00:32:00 -0700

    Time: 85 ms (33.01%) | Memory: 20 MB (8.21%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class HeapNode:
+    def __init__(self, node):
+        self.node = node
+    def __lt__(self, other):
+        return self.node.val < other.node.val 
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy 
+        heap = []
+
+        for l in lists:
+            if l: 
+                 heapq.heappush(heap, HeapNode(l))
+        
+        while heap:
+            heapNode = heapq.heappop(heap)
+            node = heapNode.node
+            curr.next = node
+            curr = curr.next
+            if node.next:
+                heapq.heappush(heap, HeapNode(node.next))
+        return dummy.next
