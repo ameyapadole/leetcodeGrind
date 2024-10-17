fcdd163b9681b3e16379abc21689e6957cc3b9f4
Ameya Padole <ameyapadole26@gmail.com>
2024-10-16 23:14:39 -0700

    Time: 80 ms (49.38%) | Memory: 19.4 MB (57.88%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,27 +1,28 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode: 
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = None 
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0 or not lists:
+        
+        if not lists or len(lists) == 0:
             return None
-
+        
         while len(lists) > 1:
-            mergedLists = [] 
+            mergedLists = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if (i + 1) < len(lists) else None
                 mergedLists.append(self.mergeList(l1, l2))
             lists = mergedLists
         return lists[0]
-        
+ 
     def mergeList(self, l1, l2):
         dummy = ListNode()
-        tail = dummy
+        tail = dummy 
         while l1 and l2: 
-            if l1.val < l2.val:
+            if l1.val < l2.val: 
                 tail.next = l1
                 l1 = l1.next
             else:
@@ -30,7 +31,6 @@ class Solution:
             tail = tail.next
         if l1:
             tail.next = l1
-        if l2: 
+        if l2:
             tail.next = l2
         return dummy.next
-        
