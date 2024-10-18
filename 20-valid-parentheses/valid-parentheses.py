7f1b05b229aa47fe4891260ba0fcafc9e129110c
Ameya Padole <ameyapadole26@gmail.com>
2024-10-18 15:07:08 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (22.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,17 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        mapping = {")":"(", 
-                   "}":"{",
-                   "]": "[" }
-        stack = deque()
+        hashmap = {")":"(", "}":"{", "]":"["}
+        stack = []
 
         for char in s: 
-            if char in mapping: 
+            if char in hashmap:
                 top_element = stack.pop() if stack else '#'
 
-                if mapping[char] != top_element:
-                    return False 
+                if hashmap[char] != top_element: 
+                    return False
             else:
                 stack.append(char)
 
-        return not stack 
+        return not stack
+        
