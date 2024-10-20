5dd525c621a1cce4fa09d7f31441fdc2d0f525e0
Ameya Padole <ameyapadole26@gmail.com>
2024-10-19 17:33:19 -0700

    Time: 43 ms (99.18%) | Memory: 18.2 MB (59.98%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        output =[]
+        indexRemove = set()
+
+        for i, c in enumerate(s):
+            if c not in "()":
+                continue
+            if c == "(":
+                stack.append(i)
+            elif not stack:
+                indexRemove.add(i)
+            else:
+                stack.pop()
+        
+        indexRemove = indexRemove.union(set(stack))
+
+        for i, c in enumerate(s):
+            if i not in indexRemove:
+                output.append(c)
+
+        return "".join(output)
+        
