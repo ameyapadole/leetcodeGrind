5b8110a878f10782366a01a9109e653baac06089
Ameya Padole <ameyapadole26@gmail.com>
2024-10-19 17:31:03 -0700

    Time: 67 ms (98.81%) | Memory: 21 MB (16.00%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        n = len(s) 
+        num = 0 
+        operation = "+"
+
+        if not s:
+            return 0
+        
+        for i, c in enumerate(s):
+            if c.isdigit():
+                num = num * 10 + int(c)
+
+            if not c.isdigit() and not c.isspace() or i == n - 1:
+                if operation == "+":
+                    stack.append(num)
+                elif operation == "-":
+                    stack.append(-num)
+                elif operation == "*":
+                    stack.append(stack.pop() * num)        
+                elif operation == "/":
+                    stack.append(int(stack.pop() / num ))
+                operation = c
+                num = 0 
+        return sum(stack)
