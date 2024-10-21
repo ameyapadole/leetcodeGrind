7b9719929fe9823555f519c667ee6fdf9129cec7
Ameya Padole <ameyapadole26@gmail.com>
2024-10-20 18:25:10 -0700

    Time: 4 ms (96.26%) | Memory: 16.6 MB (18.51%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -17,11 +17,12 @@ class Solution:
             node, cols = queue.popleft()
             if node is not None:
                 columnTable[cols].append(node.val)
-                min_cols = min(cols, min_cols)
-                max_cols = max(cols, max_cols)
-
+                
                 queue.append((node.left, cols - 1))
                 queue.append((node.right, cols + 1))
+
+                min_cols = min(cols, min_cols)
+                max_cols = max(cols, max_cols)
 
         return [columnTable[i] for i in range(min_cols, max_cols + 1)]
 
