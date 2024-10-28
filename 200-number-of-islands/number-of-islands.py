100403bf7641eec71821c17ea8a46e1dde386749
Ameya Padole <ameyapadole26@gmail.com>
2024-10-28 00:05:37 -0700

    Time: 236 ms (68.99%) | Memory: 19 MB (68.92%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,21 +1,32 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        res = 0
         if not grid:
             return 0 
+        res = 0 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == '1':
-                        res += 1
-                        self.dfs(grid, r, c)
-        return res 
+                    res += 1
+                    self.dfs(grid, r, c)
+        return res
+
     
     def dfs(self, grid, r, c):
-        if (r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == "0"): #Checking out of bounds
-            return
-        grid[r][c] = "0" # Mark cell as visited 
+        
+        if (
+            r < 0 or
+            r >= len(grid) or
+            c < 0 or 
+            c >= len(grid[0]) or
+            grid[r][c] == '0'
+        ): return
+
+        grid[r][c] = '0'
         self.dfs(grid, r + 1, c)
         self.dfs(grid, r - 1, c)
         self.dfs(grid, r, c + 1)
         self.dfs(grid, r, c - 1)
 
+
+
+        
