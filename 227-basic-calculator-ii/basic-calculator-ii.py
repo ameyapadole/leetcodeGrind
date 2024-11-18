7cdc9bae8b2c7e1516d085a96ee3caac7e5b2f0b
Ameya Padole <ameyapadole26@gmail.com>
2024-11-17 20:04:35 -0800

    Time: 61 ms (76.09%) | Memory: 18 MB (90.26%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,23 +1,31 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        number = 0
-        operator = '+'
-        n = len(s)
-
-        for i, c in enumerate(s):
-            if c.isdigit():
-                number = number * 10 + int(c)
-            if not c.isdigit() and not c.isspace() or i == n - 1:
-                if operator == '+':
-                    stack.append(number)
-                elif operator == '-':
-                    stack.append(-number)
-                elif operator == '*':
-                    stack.append(stack.pop() * number)
-                elif operator == '/':
-                    stack.append(int(stack.pop() / number))
-                operator = c 
-                number = 0
-        return sum(stack)
+        if not s:
+            return 0
         
+        current_number = 0
+        last_number = 0
+        result = 0
+        operation = '+'
+        
+        for i, current_char in enumerate(s):
+            if current_char.isdigit():
+                current_number = (current_number * 10) + int(current_char)
+            
+            if (not current_char.isdigit() and not current_char.isspace()) or i == len(s) - 1:
+                if operation == '+':
+                    result += last_number
+                    last_number = current_number
+                elif operation == '-':
+                    result += last_number
+                    last_number = -current_number
+                elif operation == '*':
+                    last_number = last_number * current_number
+                elif operation == '/':
+                    last_number = int(last_number / current_number)  # Integer division
+                
+                operation = current_char
+                current_number = 0
+        
+        result += last_number
+        return result
