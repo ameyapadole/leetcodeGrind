c6df704e77a2626fe316ec4658ce830471f0c107
Ameya Padole <ameyapadole26@gmail.com>
2024-11-17 16:22:07 -0800

    Time: 66 ms (66.72%) | Memory: 20.6 MB (40.12%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -7,17 +7,17 @@ class Solution:
         for i, c in enumerate(s):
             if c.isdigit():
                 number = number * 10 + int(c)
-            if not c.isdigit() and not c.isspace() or i == n - 1:   
-                if operator == '+':
+            if not c.isdigit() and not c.isspace() or i == n - 1:
+                if operator == '+': 
                     stack.append(number)
+
                 elif operator == '-':
                     stack.append(-number)
                 elif operator == '/':
-                    stack.append(int(stack.pop()/number))
+                    stack.append(int(stack.pop()/ number))
                 elif operator == '*':
                     stack.append(stack.pop() * number)
-                operator = c
-                number = 0
-        
+                operator = c 
+                number = 0        
         return sum(stack)
         
