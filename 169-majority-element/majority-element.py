27726a8171e9eb3955dc2b34935f52780d1e99c0
Ameya Padole <ameyapadole26@gmail.com>
2025-01-03 07:20:49 -1000

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.30%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -6,9 +6,14 @@ class Solution:
 
         for num in nums:
             if count == 0:
-                candidate = num
+                candidate = num  
+        
+
             if num == candidate:
                 count += 1
+                
             else:
                 count -= 1
+       
+
         return candidate
