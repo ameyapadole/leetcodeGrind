4d1313eb68bef49d00d72d254d76acdc1a06341c
Ameya Padole <ameyapadole26@gmail.com>
2025-01-04 00:21:08 -1000

    Time: 15 ms (16.16%) | Memory: 18.3 MB (10.03%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -3,10 +3,11 @@ class Solution:
         count = 0 
         for i in range(len(flowerbed)):
             if flowerbed[i] == 0:
-                l = (i ==0) or (flowerbed[i - 1] == 0)
-                r = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+                l = (i == 0) or flowerbed[i - 1] == 0
+                r = (i == len(flowerbed) - 1) or flowerbed[i + 1] == 0
 
                 if l and r:
                     flowerbed[i] = 1
                     count += 1
-        return count >= n        
+                    
+        return count >= n
