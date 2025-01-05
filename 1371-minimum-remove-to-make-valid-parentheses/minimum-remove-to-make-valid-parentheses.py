cbedd974204b0ad16260fa614d5809021b5a8bd3
Ameya Padole <ameyapadole26@gmail.com>
2025-01-05 04:34:26 -1000

    Time: 47 ms (43.96%) | Memory: 19.8 MB (14.53%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -4,20 +4,20 @@ class Solution:
         res = []
         indexRemove = set()
 
-        for i, c in enumerate(s):
+        for i,c in enumerate(s):
             if c not in "()":
-                continue 
+                continue
             if c == "(":
                 stack.append(i)
             elif not stack:
                 indexRemove.add(i)
             else:
                 stack.pop()
-        
+            
         indexRemove = indexRemove.union(set(stack))
 
-        for i,c in enumerate(s):
+        for i, c in enumerate(s):
             if i not in indexRemove:
                 res.append(c)
-        return "".join(res)
         
+        return "".join(res)
