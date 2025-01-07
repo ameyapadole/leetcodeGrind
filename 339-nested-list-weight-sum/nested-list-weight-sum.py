5841c2f0908423fd6f71b80175df17571251ef5c
Ameya Padole <ameyapadole26@gmail.com>
2025-01-07 05:41:31 -1000

    Time: 0 ms (100.00%) | Memory: 18.2 MB (7.97%) - LeetSync

--- 339-nested-list-weight-sum/nested-list-weight-sum.py
+++ 339-nested-list-weight-sum/nested-list-weight-sum.py
@@ -43,8 +43,8 @@
 
 class Solution:
     def depthSum(self, nestedList: List[NestedInteger]) -> int:
-        def dfs(nestList, depth): 
-            total = 0
+        def dfs(nestList, depth):
+            total = 0 
             for nest in nestList:
                 if nest.isInteger():
                     total += nest.getInteger() * depth
@@ -52,6 +52,5 @@ class Solution:
                     total += dfs(nest.getList(), depth + 1)
             return total 
         return dfs(nestedList, 1)
-            
 
         
