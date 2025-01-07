b316d5d891c6988559429561f6c603eb7e062f2b
Ameya Padole <ameyapadole26@gmail.com>
2025-01-07 01:01:59 -1000

    Time: 38 ms (93.22%) | Memory: 28.9 MB (16.55%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -3,6 +3,7 @@ class Solution:
         def quickselect(nums, k):
             pivot = random.choice(nums)
             l, m, r = [], [], []
+
             for num in nums:
                 if num > pivot:
                     l.append(num)
@@ -10,10 +11,12 @@ class Solution:
                     r.append(num)
                 else:
                     m.append(num)
+
             if len(l) >= k:
                 return quickselect(l, k)
+
             if len(l) + len(m) < k:
                 return quickselect(r, k - len(l) - len(m))
+
             return pivot
-        return quickselect(nums, k)
-        
+        return quickselect(nums, k)        
