96ed854ed4c7024535648225a5541264f09a40f1
Ameya Padole <ameyapadole26@gmail.com>
2025-01-07 06:55:31 -1000

    Time: 60 ms (71.58%) | Memory: 22.2 MB (52.14%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,16 +1,33 @@
+import random
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        minHeap = []
-        res = []
+        def quickselect(points, k):
+            pivot = random.choice(points)
+            pivot_dist = squared_distance(pivot)
+            closer, equal, farther = [], [], []
 
-        for x, y in points:
-            distance = (x ** 2) + (y ** 2)
-            minHeap.append([distance, x, y])
-        heapq.heapify(minHeap)
+            # Partition points into three groups
+            for point in points:
+                dist = squared_distance(point)
+                if dist < pivot_dist:
+                    closer.append(point)
+                elif dist > pivot_dist:
+                    farther.append(point)
+                else:
+                    equal.append(point)
 
-        while k > 0:
-            distance, x, y = heapq.heappop(minHeap)
-            res.append([x, y])
-            k -= 1
-        return res 
-        
+            # Determine where k falls
+            if len(closer) >= k:
+                return quickselect(closer, k)  # k closest points are in `closer`
+            elif len(closer) + len(equal) < k:
+                # Need to include points from `farther`
+                return closer + equal + quickselect(farther, k - len(closer) - len(equal))
+            else:
+                # The k closest points are in `closer + equal`
+                return closer + equal
+
+        def squared_distance(point):
+            return point[0] ** 2 + point[1] ** 2
+
+        return quickselect(points, k)
