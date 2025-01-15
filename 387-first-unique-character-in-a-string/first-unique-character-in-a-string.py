bd79e702ef518b2689696b9d53cfa1d7ba479b55
Ameya Padole <ameyapadole26@gmail.com>
2025-01-14 22:50:59 -1000

    Time: 78 ms (25.18%) | Memory: 18.2 MB (19.07%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,17 +1,17 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        
-        char_map = {}
+        charMap = {}
 
-        # Build the hashmap with character counts
         for char in s:
-            if char in char_map:
-                char_map[char] += 1
+            if char in charMap:
+                charMap[char] += 1
             else:
-                char_map[char] = 1
-
-        # Iterate through the string again to find the first repeating character
+                charMap[char] = 1
+        
         for index, char in enumerate(s):
-            if char_map[char] == 1:
-                return index  # Return the index of the first repeated character
-        return -1 
+            if charMap[char] == 1:
+                return index
+        
+        return -1
+            
+        
